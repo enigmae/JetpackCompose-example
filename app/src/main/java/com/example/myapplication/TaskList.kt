d023c7e33ff2dba9e7eff73ea49b9c5dd1fd689e
davidg <dogriffiths@gmail.com>
2021-12-07 16:57:35 +0000

    Changes: Tue  7 Dec 2021 16:57:35 GMT

--- app/src/main/java/com/example/myapplication/TaskList.kt
+++ app/src/main/java/com/example/myapplication/TaskList.kt
@@ -2,19 +2,30 @@ package com.example.myapplication
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun TaskList(tasks: List<Task>) {
     LazyColumn {
         items(tasks.size) {
             val task = tasks[it]
-            Row {
-                Text(task.name)
-                Text(if (task.complete) "  ✓" else "")
-            }
+            TaskItem(task)
         }
+    }
+}
+
+@Composable
+private fun TaskItem(task: Task) {
+    Row(
+        modifier = Modifier.fillMaxWidth().padding(16.dp),
+    ) {
+        Text(task.name)
+        Text(if (task.complete) "  ✓" else "")
     }
 }
