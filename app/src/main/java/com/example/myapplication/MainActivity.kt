021a480ef57366e89562171f1880c42fe7414775
davidg <dogriffiths@gmail.com>
2021-12-07 15:26:24 +0000

    Changes: Tue  7 Dec 2021 15:26:24 GMT

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
@@ -26,10 +27,12 @@ fun TaskEditor() {
             value = "Buy bread",
             onValueChange = {},
         )
-        Checkbox(
-            checked = true,
-            onCheckedChange = {},
-        )
-        Text("Complete?")
+        Row {
+            Checkbox(
+                checked = true,
+                onCheckedChange = {},
+            )
+            Text("Complete?")
+        }
     }
 }
