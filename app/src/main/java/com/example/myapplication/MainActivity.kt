35a380d6d6b3a0a39cd631b1eee5c2c411c7a4d7
davidg <dogriffiths@gmail.com>
2021-12-07 18:44:39 +0000

    Changes: Tue  7 Dec 2021 18:44:39 GMT

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,50 +3,14 @@ package com.example.myapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.navigation.NavType
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
-import androidx.navigation.navArgument
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        val taskDao = TaskDao()
         val taskDao = TaskDatabase.getInstance(this).taskDao
         val vmf = MyViewModelFactory(taskDao)
         setContent {
-            val navController = rememberNavController()
-
-            NavHost(navController = navController, startDestination = "/tasks") {
-                composable("/tasks") {
-                    TasksScreen(vmf, onAddTask = {
-                        navController.navigate("/tasks/new")
-                    },
-                        onClickTask = {
-                            navController.navigate("/tasks/${it.id}")
-                        }
-                    )
-                }
-                composable("/tasks/new") {
-                    AddTaskScreen(vmf, onTaskSaved = {
-                        navController.navigate("/tasks")
-                    })
-                }
-                composable(
-                    "/tasks/{taskId}",
-                    arguments = listOf(navArgument("taskId") {
-                        type = NavType.LongType
-                    })
-                ) {
-                    it.arguments?.let {
-                        val taskId = it.get("taskId") as Long
-                        EditTaskScreen(vmf, taskId = taskId, onTaskSaved = {
-                            navController.navigate("/tasks")
-                        })
-                    }
-                }
-            }
+            MainNavigation(vmf)
         }
     }
 }
---
+++ app/src/main/java/com/example/myapplication/MainNavigation.kt
@@ -0,0 +1,64 @@
+package com.example.myapplication
+
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+
+@Composable
+fun MainNavigation(vmf: MyViewModelFactory) {
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = "/tasks") {
+        composable("/tasks") {
+            MyScaffold(title = "Tasks!") {
+                TasksScreen(vmf, onAddTask = {
+                    navController.navigate("/tasks/new")
+                },
+                    onClickTask = {
+                        navController.navigate("/tasks/${it.id}")
+                    }
+                )
+            }
+        }
+        composable("/tasks/new") {
+            AddTaskScreen(vmf, onTaskSaved = {
+                navController.navigate("/tasks")
+            })
+        }
+        composable(
+            "/tasks/{taskId}",
+            arguments = listOf(navArgument("taskId") {
+                type = NavType.LongType
+            })
+        ) {
+            it.arguments?.let {
+                val taskId = it.get("taskId") as Long
+                EditTaskScreen(vmf, taskId = taskId, onTaskSaved = {
+                    navController.navigate("/tasks")
+                })
+            }
+        }
+    }
+}
+
+@Composable
+fun MyScaffold(
+    title: String,
+    content: @Composable () -> Unit
+) {
+    Scaffold(
+        topBar = {
+            TopAppBar(title = {
+                Text(title)
+            })
+        }
+    ) {
+        content()
+    }
+}
