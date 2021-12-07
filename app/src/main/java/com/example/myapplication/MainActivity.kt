8708d7fb10ce0775ecf73543879c74d6766ad307
davidg <dogriffiths@gmail.com>
2021-12-07 15:37:48 +0000

    Changes: Tue  7 Dec 2021 15:37:48 GMT

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -5,12 +5,14 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,6 +31,7 @@ fun TaskEditor() {
     Column {
         TextField(
             value = name,
+            modifier = Modifier.fillMaxWidth(),
             onValueChange = {
                 name = it
             },
