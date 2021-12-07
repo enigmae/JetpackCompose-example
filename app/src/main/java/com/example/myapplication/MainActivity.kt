dd25b945fba0d2ffe42fe3bb64a7aa763bd83d7f
davidg <dogriffiths@gmail.com>
2021-12-07 18:11:26 +0000

    Changes: Tue  7 Dec 2021 18:11:26 GMT

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,6 +3,9 @@ package com.example.myapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,7 +14,16 @@ class MainActivity : ComponentActivity() {
         val taskDao = TaskDatabase.getInstance(this).taskDao
         val vmf = MyViewModelFactory(taskDao)
         setContent {
-            TasksScreen(vmf)
+            val navController = rememberNavController()
+
+            NavHost(navController = navController, startDestination = "/tasks") {
+                composable("/tasks") {
+                    TasksScreen(vmf)
+                }
+                composable("/tasks/new") {
+                    AddTaskScreen(vmf)
+                }
+            }
         }
     }
 }
